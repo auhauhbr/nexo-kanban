f9d19bca7758482132e0007ae57ff0cabd91e9e8
Jefferson Tadeu <tadeu.santos7148@gmail.com>
2026-06-12 19:38:43 -0300

    ci: tirar dps, mas pro pages funciona

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 ﻿import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { Aplicacao } from "./Aplicacao";
 import { ProvedorAutenticacao } from "./contexto/ContextoAutenticacao";
@@ -13,13 +13,13 @@ const clienteConsultas = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={clienteConsultas}>
-      <BrowserRouter>
+      <HashRouter>
         <ProvedorAutenticacao>
           <ProvedorNotificacoes>
             <Aplicacao />
           </ProvedorNotificacoes>
         </ProvedorAutenticacao>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>
 );
